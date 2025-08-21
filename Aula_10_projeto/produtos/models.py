e150e6da62aa2ee9ae397a2dd788019c47d71bdc
Guilhermezx <guilhermeytb.08@gmail.com>
2025-08-20 21:58:38 -0300

    Aula 10

---
+++ Aula_10_projeto/produtos/models.py
@@ -0,0 +1,17 @@
+# produtos/models.py (aula 10)
+
+from django.db import models
+
+##
+# Primeira versão do model adicionado na aula 10
+##
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.PositiveIntegerField()
+    imagem = models.ImageField(upload_to='produtos/', blank=True, null=True) # Campo para imagem
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
