79d1ca9d29c2701666805ec1cb122cfa79e3d7a3
Guilhermezx <guilhermeytb.08@gmail.com>
2025-10-16 20:51:03 -0300

    aula15.2

---
+++ loja_eletronicos-fase02-crud-produtos(1)/loja_eletronicos/loja/forms.py
@@ -0,0 +1,24 @@
+# loja/forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Perfil
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="E-mail válido")
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Perfil
+        fields = ("imagem",)
